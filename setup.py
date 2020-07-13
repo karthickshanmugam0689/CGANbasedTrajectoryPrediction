8b03a95671453989f0ee58ac2ae5e1d882327df4
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2020-07-14 01:27:58 +0200

    master: add setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='csgan',
+      version='0.1',
+      description='Conditional GAN for speed simulation in street scenes',
+      author='Vishal Sowrirajan',
+      packages=['csgan'],
+      zip_safe=False)

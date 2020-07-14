d83b18530143775ef76d689bbb61ab14e6a4985d
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2020-07-15 01:40:56 +0200

    master: update requirements.txt file

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    if f.startswith("#"):
-        pass
-    requirements = f.read().splitlines()
-
 setup(name='csgan',
       version='0.1',
       description='Conditional GAN for speed simulation in street scenes',
       author='Vishal Sowrirajan',
       license='MIT',
-      install_requires=requirements,
       packages=['csgan', 'csgan.data', 'scripts'],
       zip_safe=False)
